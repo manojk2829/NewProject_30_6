cc0e5f3c61cdad0c9f7fc1d4842fe307e21a038e
aniket <aniket@DESKTOP-HN0PUSP>
2018-06-30 09:50:08 +0530

    Project_Artifact_30_6_new_Location

---
+++ Project_Artifact/src/main/java/package_Group/Project_Artifact/App.java
@@ -0,0 +1,13 @@
+package package_Group.Project_Artifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
